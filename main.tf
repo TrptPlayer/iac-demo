356f5341761b2e7b4bb673a85b862f4f7cbb9524
TrptPlayer <jqfree@gmail.com>
2017-10-06 20:32:12 -0400

    Add a .gitignore file

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-d651b8ac"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "terraform-example"
+  }
+}
